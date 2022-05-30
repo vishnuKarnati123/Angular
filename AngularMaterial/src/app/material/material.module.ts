3624451ceeb40ecf69e3694ab710380c57c81c2e
vkarna654 <VishnuvardhanReddy_Karnati@comcast.com>
2022-05-30 17:15:51 +0530

    Angular Material

---
+++ AngularMaterial/src/app/material/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+
+const MaterialComponents=[
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule
+];
+@NgModule({
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+})
+export class MaterialModule { }
